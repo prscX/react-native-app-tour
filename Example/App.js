f6b0771117757d4763ac9fe1676a6f5c23155f67
cong.nguyen <cong.nguyen@enouvo.com>
2018-03-17 11:15:37 +0700

    - update example: now can listen event (next & finish)

--- Example/App.js
+++ Example/App.js
@@ -4,24 +4,27 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Button } from "react-native";
-
+import React, { Component } from 'react';
 import {
-  AppTour,
-  AppTourSequence,
-  AppTourView
-} from "react-native-app-tour";
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  DeviceEventEmitter,
+} from 'react-native';
 
-import Top from "./components/Top";
-import Center from "./components/Center";
-import Bottom from "./components/Bottom";
+import { AppTour, AppTourSequence, AppTourView } from 'react-native-app-tour';
+
+import Top from './components/Top';
+import Center from './components/Center';
+import Bottom from './components/Bottom';
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
@@ -29,6 +32,11 @@ export default class App extends Component<{}> {
     super(props);
 
     this.appTourTargets = [];
+  }
+
+  componentWillMount() {
+    this.registerSequenceStepEvent();
+    this.registerFinishSequenceEvent();
   }
 
   componentDidMount() {
@@ -40,6 +48,30 @@ export default class App extends Component<{}> {
       AppTour.ShowSequence(appTourSequence);
     }, 1000);
   }
+
+  registerSequenceStepEvent = () => {
+    if (this.sequenceStepListener) {
+      this.sequenceStepListener.remove();
+    }
+    this.sequenceStepListener = DeviceEventEmitter.addListener(
+      'onShowSequenceStepEvent',
+      (e: Event) => {
+        console.log(e);
+      }
+    );
+  };
+
+  registerFinishSequenceEvent = () => {
+    if (this.finishSequenceListener) {
+      this.finishSequenceListener.remove();
+    }
+    this.finishSequenceListener = DeviceEventEmitter.addListener(
+      'onFinishSequenceEvent',
+      (e: Event) => {
+        console.log(e);
+      }
+    );
+  };
 
   render() {
     return (
@@ -70,16 +102,16 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between"
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
   top: {
-    flex: 1
+    flex: 1,
   },
   center: {
-    flex: 1
+    flex: 1,
   },
   bottom: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
