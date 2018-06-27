485adf43a21a484d2475ac582d45caec98513a96
Pranav Raj Singh Chauhan <pranav.chauhan@metricstream.com>
2018-06-27 14:06:59 +0530

    - android target radius fix

--- android/src/main/java/ui/apptour/RNAppTourModule.java
+++ android/src/main/java/ui/apptour/RNAppTourModule.java
@@ -38,7 +38,6 @@ import com.getkeepsafe.taptargetview.TapTarget;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class RNAppTourModule extends ReactContextBaseJavaModule {
 
     public RNAppTourModule(ReactApplicationContext reactContext) {
@@ -50,12 +49,8 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
         return "RNAppTour";
     }
 
-    private void sendEvent(ReactContext reactContext,
-                           String eventName,
-                           @Nullable WritableMap params) {
-        reactContext
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(eventName, params);
+    private void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
     }
 
     @ReactMethod
@@ -77,7 +72,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
                             int view = views.getInt(i);
 
                             View refView = nativeViewHierarchyManager.resolveView(view);
-//                            View refView = getCurrentActivity().findViewById(view);
+                            // View refView = getCurrentActivity().findViewById(view);
 
                             TapTarget tapTarget = generateTapTarget(refView, props.getMap(String.valueOf(view)));
                             targetViews.add(tapTarget);
@@ -85,7 +80,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
 
                         TapTargetSequence tapTargetSequence = new TapTargetSequence(dialog).targets(targetViews);
                         tapTargetSequence.considerOuterCircleCanceled(true);
- 
+
                         tapTargetSequence.listener(new TapTargetSequence.Listener() {
                             @Override
                             public void onSequenceFinish() {
@@ -115,8 +110,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
 
                                 sendEvent(getReactApplicationContext(), "onCancelStepEvent", params);
                             }
-                        })
-                                .continueOnCancel(true);
+                        }).continueOnCancel(true);
                         tapTargetSequence.start();
                     }
                 });
@@ -138,7 +132,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
                     @Override
                     public void execute(NativeViewHierarchyManager nativeViewHierarchyManager) {
                         View refView = nativeViewHierarchyManager.resolveView(view);
-//                        View refView = getCurrentActivity().findViewById(view);
+                        // View refView = getCurrentActivity().findViewById(view);
 
                         TapTarget targetView = generateTapTarget(refView, props);
 
@@ -149,15 +143,16 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
         });
     }
 
-
-    public static class TapTargetViewListener extends  TapTargetView.Listener {
+    public static class TapTargetViewListener extends TapTargetView.Listener {
         private ReadableMap props;
 
-        public TapTargetViewListener (ReadableMap props) {
+        public TapTargetViewListener(ReadableMap props) {
             this.props = props;
         }
 
-        /** Signals that the user clicked on the outer circle portion of the tap target **/
+        /**
+         * Signals that the user clicked on the outer circle portion of the tap target
+         **/
         @Override
         public void onOuterCircleClick(TapTargetView view) {
             // no-op as default
@@ -173,7 +168,6 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
             }
         }
     }
-
 
     @TargetApi(18)
     private TapTarget generateTapTarget(final View view, final ReadableMap props) {
@@ -210,8 +204,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
             dimColor = props.getString("dimColor");
         }
 
-
-        //Other Props
+        // Other Props
         float outerCircleAlpha = 0.96f;
         int titleTextSize = 20;
         int descriptionTextSize = 10;
@@ -219,7 +212,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
         boolean cancelable = true;
         boolean tintTarget = true;
         boolean transparentTarget = true;
-        int targetRadius = 60;
+        int targetRadius = 44;
 
         try {
             outerCircleAlpha = (float) props.getDouble("outerCircleAlpha");
@@ -254,9 +247,8 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
         } catch (Exception e) {
         }
 
-
-        //Populate Props
-//        TapTarget targetView = TapTarget.forView(view, title, description);
+        // Populate Props
+        // TapTarget targetView = TapTarget.forView(view, title, description);
         int[] points = new int[2];
         view.getLocationOnScreen(points);
         Rect rectBonds = new Rect(points[0], points[1], points[0] + view.getWidth(), points[1] + view.getHeight());
@@ -275,7 +267,6 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
             targetView.textColorInt(Color.parseColor(textColor));
         if (dimColor != null && dimColor.length() > 0)
             targetView.dimColorInt(Color.parseColor(dimColor));
-
 
         targetView.outerCircleAlpha(outerCircleAlpha);
         targetView.titleTextSize(titleTextSize);
